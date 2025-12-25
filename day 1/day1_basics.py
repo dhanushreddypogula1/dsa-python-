51e136e82ba157452f35930a466f10ac9da16e06
dhanushreddypogula1 <dhanushreddypogula1@gmail.com>
2025-12-25 20:27:13 +0530

    Add files via upload

---
+++ day 1/day1_basics.py
@@ -0,0 +1,23 @@
+#Variables
+
+a,b=10,20
+
+print(a+b)
+
+
+#Data Types
+name="AIML"
+pi =3.14
+is_active=True
+
+print(type(name),type(pi),type(is_active))
+
+#loop
+for i in range(1,6):
+    print(i)
+
+#Function
+def add(x,y):
+    print(x+y)
+
+print(add(3,7))
