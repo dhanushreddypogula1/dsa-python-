4519b644e7432b66334230780508a53090a489cf
dhanushreddypogula1 <dhanushreddypogula1@gmail.com>
2025-12-25 20:27:53 +0530

    Add files via upload

---
+++ day 2/day2_lists.py
@@ -0,0 +1,15 @@
+nums = [1, 2, 3, 4, 5]
+
+# Accessing
+print(nums[0],nums[-1])      
+
+# Appending
+nums.append(6)
+print(nums)
+
+# loop
+for num in nums:
+    print(num*2)
+
+# length
+print(len(nums))
---
+++ day 3/day3_timecomplexity.py
@@ -0,0 +1,13 @@
+nums=[1,2,3,4]
+
+# Time Complexity: O(n)
+for num in nums:
+    print(num)
+
+# Time Complexity: O(1)
+print("Done")
+
+# Time Complexity: O(n^2)
+for i in nums:
+    for j in nums:
+        print(i, j)
